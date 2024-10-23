fcd27bea253b5afd15fe17200f277df9145c00b7
Henning Sieh <kontakt@henningsieh.de>
2024-10-23 11:57:54 +0200

    deleted unused / clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,6 @@ import {
   LogOut,
   MoreHorizontal,
   PencilRuler,
-  Plus,
   Settings,
   Sparkles,
   Sprout,
@@ -47,7 +46,6 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { Separator } from "~/components/ui/separator";
@@ -74,7 +72,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "~/components/theme-provider";
 import { SidebarProvider, SidebarInset } from "~/components/ui/sidebar";
-import { useState } from "react";
+import { TeamSwitcher } from "~/components/ui/sidebar/team-switcher";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -256,8 +254,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [activeTeam, setActiveTeam] = useState(data.teams[0]);
-
   return (
     <html lang="en">
       <body
@@ -272,65 +268,7 @@ export default function RootLayout({
           <SidebarProvider>
             <Sidebar collapsible="icon">
               <SidebarHeader>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton
-                          size="lg"
-                          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                        >
-                          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                            <activeTeam.logo className="size-4" />
-                          </div>
-                          <div className="grid flex-1 text-left text-sm leading-tight">
-                            <span className="truncate font-semibold">
-                              {activeTeam.name}
-                            </span>
-                            <span className="truncate text-xs">
-                              {activeTeam.plan}
-                            </span>
-                          </div>
-                          <ChevronsUpDown className="ml-auto" />
-                        </SidebarMenuButton>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent
-                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-                        align="start"
-                        side="bottom"
-                        sideOffset={4}
-                      >
-                        <DropdownMenuLabel className="text-xs text-muted-foreground">
-                          Teams
-                        </DropdownMenuLabel>
-                        {data.teams.map((team, index) => (
-                          <DropdownMenuItem
-                            key={team.name}
-                            onClick={() => setActiveTeam(team)}
-                            className="gap-2 p-2"
-                          >
-                            <div className="flex size-6 items-center justify-center rounded-sm border">
-                              <team.logo className="size-4 shrink-0" />
-                            </div>
-                            {team.name}
-                            <DropdownMenuShortcut>
-                              ⌘{index + 1}
-                            </DropdownMenuShortcut>
-                          </DropdownMenuItem>
-                        ))}
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem className="gap-2 p-2">
-                          <div className="flex size-6 items-center justify-center rounded-md border bg-background">
-                            <Plus className="size-4" />
-                          </div>
-                          <div className="font-medium text-muted-foreground">
-                            Add team
-                          </div>
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </SidebarMenuItem>
-                </SidebarMenu>
+                <TeamSwitcher teams={data.teams} />
               </SidebarHeader>
               <SidebarContent>
                 <SidebarGroup>
