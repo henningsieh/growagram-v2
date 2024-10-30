6323d23c9eef8e2e774ac065c36ae5d3185c150e
Henning Sieh <kontakt@henningsieh.de>
2024-10-30 15:49:45 +0100

    feat: formatDate util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type DateFormatOptions = {
+  month?: "long" | "short" | "2-digit";
+  weekday?: "long" | "short";
+  includeYear?: boolean;
+};
+
+export function formatDate<Locale extends string = "en" | "de">(
+  date: Date,
+  locale: Locale,
+  options: DateFormatOptions,
+): string {
+  const { month = "short", weekday, includeYear = true } = options;
+
+  const formatOptions: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    weekday: weekday,
+    month: month,
+    year: includeYear ? "numeric" : undefined,
+  };
+
+  return new Intl.DateTimeFormat(locale, formatOptions).format(date);
+}
