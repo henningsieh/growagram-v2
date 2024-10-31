a927700012f1f1d88efba06d136ce5902e8c84cc
Henning Sieh <kontakt@henningsieh.de>
2024-10-31 05:37:55 +0100

    fix: href translation by using { Link } from "~/lib/i18n/routing"

--- src/components/layouts/navbar/desktop-nav/list-item.tsx
+++ src/components/layouts/navbar/desktop-nav/list-item.tsx
@@ -1,5 +1,6 @@
 import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
 import { forwardRef } from "react";
+import { Link } from "~/lib/i18n/routing";
 import { cn } from "~/lib/utils";
 
 export const ListItem = forwardRef<
@@ -9,17 +10,18 @@ export const ListItem = forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none text-accent-foreground no-underline outline-none transition-colors hover:bg-accent hover:text-foreground focus:bg-accent focus:text-accent-foreground",
             className,
           )}
+          href={props.href as string}
           {...props}
         >
           <div className="text-sm font-bold leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug">{children}</p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
