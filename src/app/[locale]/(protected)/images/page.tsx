b277265777e66a7a07e2087fb4b7ac20148f2d64
Henning Sieh <kontakt@henningsieh.de>
2024-11-02 17:05:47 +0100

    feat: tRPC getUserImages: protectedProcedure

--- src/app/[locale]/(protected)/images/page.tsx
+++ src/app/[locale]/(protected)/images/page.tsx
@@ -1,14 +1,9 @@
 // src/app/[locale]/(protected)/images/page.tsx
-import { eq } from "drizzle-orm";
-import Image from "next/image";
+import { ImageGrid } from "~/components/images/image-grid";
 import { Button } from "~/components/ui/button";
 import { auth } from "~/lib/auth";
-import { db } from "~/lib/db";
-import { images } from "~/lib/db/schema";
 import { Link } from "~/lib/i18n/routing";
-import { api } from "~/lib/trpc/react";
 import { HydrateClient } from "~/lib/trpc/server";
-import { formatDate, formatTime } from "~/lib/utils";
 
 import PageHeader from "../../../../components/layouts/page-header";
 
@@ -20,18 +15,8 @@ export default async function ImagesPage(props: {
     typeof props.params
   >);
   const session = await auth();
+
   if (!session?.user?.id) return null;
-
-  const [tRPCresult] = api.image.getUserImages.useSuspenseQuery({
-    cursor: null,
-    limit: undefined,
-  });
-  const userImages = tRPCresult.images;
-
-  // const userImages = await db.query.images.findMany({
-  //   where: eq(images.ownerId, session.user.id),
-  //   orderBy: (images, { desc }) => [desc(images.createdAt)],
-  // });
 
   return (
     <HydrateClient>
@@ -42,42 +27,7 @@ export default async function ImagesPage(props: {
         <Button asChild variant={"secondary"}>
           <Link href="/images/upload">Upload New Image</Link>
         </Button>
-
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {userImages.map((image) => (
-            <div
-              key={image.id}
-              className="overflow-hidden rounded-lg border shadow-sm"
-            >
-              <div className="relative h-48">
-                <Image
-                  src={image.imageUrl}
-                  alt=""
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <div className="p-3">
-                <p className="text-sm text-gray-500">
-                  Uploaded:{" "}
-                  {formatDate(image.createdAt, locale, {
-                    includeYear: true,
-                  })}
-                  {" at "}
-                  {formatTime(image.createdAt, locale, {
-                    includeSeconds: true,
-                  })}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {userImages.length === 0 && (
-          <p className="mt-8 text-center text-gray-500">
-            You haven&apos;t uploaded any images yet.
-          </p>
-        )}
+        <ImageGrid locale={locale} />
       </PageHeader>
     </HydrateClient>
   );
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { MainNavigationBar } from "~/components/layouts/navbar";
 import { ThemeProvider } from "~/components/layouts/theme-provider";
 import { Toaster } from "~/components/ui/toaster";
 import { routing } from "~/lib/i18n/routing";
+import { TRPCReactProvider } from "~/lib/trpc/react";
 import "~/styles/globals.css";
 
 const geistSans = localFont({
@@ -53,27 +54,29 @@ export default async function RootLayout(props: LayoutProps) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-background font-sans antialiased`}
       >
-        <NextIntlClientProvider messages={messages}>
-          <SessionProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <div className="relative flex min-h-screen flex-col">
-                <header className="sticky top-0 z-50 w-full border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background">
-                  <div className="relative flex h-14 items-center justify-center">
-                    <MainNavigationBar />
-                  </div>
-                </header>
-                <main className="flex-1">{props.children}</main>
-              </div>
+        <TRPCReactProvider>
+          <NextIntlClientProvider messages={messages}>
+            <SessionProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <div className="relative flex min-h-screen flex-col">
+                  <header className="sticky top-0 z-50 w-full border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background">
+                    <div className="relative flex h-14 items-center justify-center">
+                      <MainNavigationBar />
+                    </div>
+                  </header>
+                  <main className="flex-1">{props.children}</main>
+                </div>
 
-              <Toaster />
-            </ThemeProvider>
-          </SessionProvider>
-        </NextIntlClientProvider>
+                <Toaster />
+              </ThemeProvider>
+            </SessionProvider>
+          </NextIntlClientProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
