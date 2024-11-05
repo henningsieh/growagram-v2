8865d86eb975b5626deb7f9879ea39b4f5649b8b
Henning Sieh <kontakt@henningsieh.de>
2024-11-05 07:13:04 +0100

    refactor: json + svg

--- src/lib/navigation/index.ts
+++ src/lib/navigation/index.ts
@@ -1,6 +1,6 @@
 // src/lib/navigation/index.ts
-import navigationData from "./data.json";
-import type { NavigationData, NavigationItem } from "./types";
+import navigationData from "~/assets//navigaton.json";
+import type { NavigationData, NavigationItem } from "~/types/navigation";
 
 export type { NavigationData, NavigationItem };
 export default navigationData;
