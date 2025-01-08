48f081bdf06c0bab1e2531b8ce8aaca91cb2a4dc
Henning Sieh <admin@henningsieh.de>
2025-01-08 17:47:10 +0100

    feat: nicer login-form

--- src/components/features/Auth/login-form.tsx
+++ src/components/features/Auth/login-form.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import { LogIn } from "lucide-react";
+import { Loader2, LogIn } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useTranslations } from "next-intl";
+import * as React from "react";
 import { FaDiscord, FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Separator } from "~/components/ui/separator";
 import { Link } from "~/lib/i18n/routing";
 import { useRouter } from "~/lib/i18n/routing";
+import { cn } from "~/lib/utils";
 import { signInWithProvider } from "~/server/actions/authActions";
 
-interface LoginFormProps {
+interface LoginFormProps extends React.HTMLAttributes<HTMLDivElement> {
   callbackUrl: string;
 }
 
-export function LoginForm({ callbackUrl }: LoginFormProps) {
+export function LoginForm({
+  className,
+  callbackUrl,
+  ...props
+}: LoginFormProps) {
   const t = useTranslations("LoginPage");
   const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const formData = new FormData(event.target as HTMLFormElement);
 
     const result = await signIn("credentials", {
       email: formData.get("email")?.toString() || "",
@@ -39,100 +40,105 @@ export function LoginForm({ callbackUrl }: LoginFormProps) {
       redirect: false,
     });
 
+    setIsLoading(false);
+
     if (result?.error) {
       router.push(`/api/auth/error?error=${result.error}`);
     }
-  };
+  }
 
   return (
-    <Card className="mx-auto min-w-full">
-      <CardHeader>
-        <CardTitle className="text-2xl">{t("title")}</CardTitle>
-        <CardDescription>{t("description")}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleSubmit} className="grid gap-4">
-          <div className="my-4 space-y-3">
-            <div className="grid gap-2">
-              <Label htmlFor="email">{t("email.label")}</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">{t("password.label")}</Label>
-                <Link
-                  href="#"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  {t("password.forgot")}
-                </Link>
-              </div>
-              <Input id="password" name="password" type="password" required />
-            </div>
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              {t("email.label")}
+            </Label>
+            <Input
+              id="email"
+              placeholder={t("email.label")}
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
           </div>
-          <Button
-            variant={"primary"}
-            type="submit"
-            className="relative w-full"
-            size="lg"
-          >
-            <LogIn size={32} className="absolute left-4" />
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              {t("password.label")}
+            </Label>
+            <Input
+              id="password"
+              placeholder={t("password.label")}
+              type="password"
+              autoCapitalize="none"
+              autoComplete="current-password"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <Loader2 className="mr-2 h-5 w-5 animate-spin" />}
             {t("submit")}
           </Button>
-        </form>
-
-        <Separator className="rounded-sm bg-muted-foreground/30" />
-
-        <h4 className="font-semibold">
-          {t("social-identity-oauth-providers")}
-        </h4>
-
-        <div className="space-y-2">
-          <Button
-            onClick={() => signInWithProvider("google", callbackUrl)}
-            variant="outline"
-            className="relative w-full"
-            size="lg"
-          >
-            <FcGoogle size={32} className="absolute left-4" />
-            {t("login-with-google")}
-          </Button>
-
-          <Button
-            onClick={() => signInWithProvider("discord", callbackUrl)}
-            variant="outline"
-            className="relative w-full"
-            size="lg"
-          >
-            <FaDiscord size={32} className="absolute left-4 text-[#7289da]" />
-
-            {t("login-with-discord")}
-          </Button>
-
-          <Button
-            onClick={() => signInWithProvider("twitter", callbackUrl)}
-            variant="outline"
-            className="relative w-full"
-            size="lg"
-          >
-            <FaTwitter size={32} className="absolute left-4 text-[#1DA1F2]" />
-
-            {t("login-with-twitter")}
-          </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-          {t("signUp.text")}{" "}
-          <Link href="#" className="underline">
-            {t("signUp.link")}
-          </Link>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
         </div>
-      </CardContent>
-    </Card>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            {t("social-identity-oauth-providers")}
+          </span>
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Button
+          variant="outline"
+          type="button"
+          disabled={isLoading}
+          onClick={() => signInWithProvider("google", callbackUrl)}
+          className="w-full"
+        >
+          {isLoading ? (
+            <Loader2 className="mr-2 h-5 w-5 animate-spin" />
+          ) : (
+            <FcGoogle className="mr-2 h-4 w-4" />
+          )}{" "}
+          {t("login-with-google")}
+        </Button>
+        <Button
+          variant="outline"
+          type="button"
+          disabled={isLoading}
+          onClick={() => signInWithProvider("discord", callbackUrl)}
+          className="w-full"
+        >
+          {isLoading ? (
+            <Loader2 className="mr-2 h-5 w-5 animate-spin" />
+          ) : (
+            <FaDiscord className="mr-2 h-4 w-4 text-[#7289da]" />
+          )}{" "}
+          {t("login-with-discord")}
+        </Button>
+        <Button
+          variant="outline"
+          type="button"
+          disabled={isLoading}
+          onClick={() => signInWithProvider("twitter", callbackUrl)}
+          className="w-full"
+        >
+          {isLoading ? (
+            <Loader2 className="mr-2 h-5 w-5 animate-spin" />
+          ) : (
+            <FaTwitter className="mr-2 h-4 w-4 text-[#1DA1F2]" />
+          )}{" "}
+          {t("login-with-twitter")}
+        </Button>
+      </div>
+    </div>
   );
 }
