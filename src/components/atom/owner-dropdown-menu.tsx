8c05c2129c7be9579579bcb42b82900d8f1f3ba5
Henning Sieh <admin@henningsieh.de>
2025-01-20 16:01:45 +0100

    fix: comment out "Sociao" DropdownMenuItem

--- src/components/atom/owner-dropdown-menu.tsx
+++ src/components/atom/owner-dropdown-menu.tsx
@@ -53,7 +53,7 @@ export function OwnerDropdownMenu({
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem
+        {/* <DropdownMenuItem
           className="flex cursor-pointer items-center justify-start"
           onSelect={(e) => e.preventDefault()}
           onClick={() => setIsSocial(!isSocial)}
@@ -71,7 +71,7 @@ export function OwnerDropdownMenu({
             checked={isSocial}
             onCheckedChange={setIsSocial}
           />
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
         <DropdownMenuItem asChild>
           <Link
             target="_blank"
