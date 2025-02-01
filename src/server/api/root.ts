bd10b29a8686fc6bc72bbeb49cfca137d8744371
Henning Sieh <admin@henningsieh.de>
2025-02-01 07:02:46 +0100

    refactor: integrate TRPC SSE (Server-sent Events + subscription support), simplify tRPC context creation and enhance API structure

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,20 @@
 // src/server/api/root.ts:
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import { photoRouter } from "~/server/api/routers/image";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { observable } from "@trpc/server/observable";
+import { createCallerFactory } from "@trpc/server/unstable-core-do-not-import";
+import { RouterInput, RouterOutput } from "~/lib/trpc/react";
 
+import { channelRouter } from "./routers/channel";
 import { chatRouter } from "./routers/chat";
 import { commentRouter } from "./routers/comments";
 import { growRouter } from "./routers/grow";
+import { photoRouter } from "./routers/image";
 import { likeRouter } from "./routers/likes";
+import { messageRouter } from "./routers/message";
 import { plantRouter } from "./routers/plant";
 import { postRouter } from "./routers/post";
 import { userRouter } from "./routers/users";
+import { createTRPCRouter, publicProcedure } from "./trpc";
 
 /**
  * This is the primary router for your server.
@@ -23,24 +28,37 @@ export const appRouter = createTRPCRouter({
   photos: photoRouter,
   likes: likeRouter,
   comments: commentRouter,
-  posts: postRouter,
+  updates: postRouter,
   chat: chatRouter,
+  channel: channelRouter,
+  message: messageRouter,
+
+  healthcheck: publicProcedure.query(() => "yay!"),
+
+  randomNumber: publicProcedure.subscription(() => {
+    return observable<number>((emit) => {
+      const int = setInterval(() => {
+        emit.next(Math.random());
+      }, 500);
+      return () => {
+        clearInterval(int);
+      };
+    });
+  }),
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
-type RouterOutput = inferRouterOutputs<AppRouter>;
-type RouterInput = inferRouterInputs<AppRouter>;
 
 // postRouter
 //  OUTPUTS:
-export type GetPostsType = RouterOutput["posts"]["getAll"];
-export type GetPostType = RouterOutput["posts"]["getAll"][number];
-export type GetCreatePostOutput = RouterOutput["posts"]["create"];
+export type GetPostsType = RouterOutput["updates"]["getAll"];
+export type GetPostType = RouterOutput["updates"]["getAll"][number];
+export type GetCreatePostOutput = RouterOutput["updates"]["create"];
 
 //  INPUTS:
-export type GetPostsInput = RouterInput["posts"]["getAll"];
-export type CreatePostInput = RouterInput["posts"]["create"];
+export type GetPostsInput = RouterInput["updates"]["getAll"];
+export type CreatePostInput = RouterInput["updates"]["create"];
 
 // userRouter
 //  OUTPUTS:
@@ -137,4 +155,4 @@ export type GrowDisconnectPlantInput = RouterInput["grows"]["disconnectPlant"];
  *       ^? Post[]
  */
 
-export const createCaller = createCallerFactory(appRouter);
+export const createCaller = createCallerFactory()(appRouter);
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,16 +1,15 @@
-// src/server/api/trpc.ts:
-
 /**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
+ * This is your entry point to setup the root configuration for tRPC on the server.
+ * - `initTRPC` should only be used once per app.
+ * - We export only the functionality that we use so we can enforce which base procedures should be used
  *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
+ * Learn how to create protected base procedures and other things below:
+ * @see https://trpc.io/docs/v11/router
+ * @see https://trpc.io/docs/v11/procedures
  */
 import { TRPCError, initTRPC } from "@trpc/server";
+import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import superjson from "superjson";
-import { ZodError } from "zod";
 import { auth } from "~/lib/auth";
 import { db } from "~/lib/db";
 
@@ -24,16 +23,21 @@ import { db } from "~/lib/db";
  * This helper generates the "internals" for a tRPC context. The API handler and RSC clients each
  * wrap this and provides the required context.
  *
- * @see https://trpc.io/docs/server/context
+ * @see https://trpc.io/docs/v11/context
  */
-export const createTRPCContext = async (opts: { headers: Headers }) => {
+export const createTRPCContext = async (opts: FetchCreateContextFnOptions) => {
   const session = await auth();
+
+  console.log("createContext for", session?.user?.name ?? "unknown user");
+
   return {
     db,
     session,
     ...opts,
   };
 };
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
 
 /**
  * 2. INITIALIZATION
@@ -42,17 +46,26 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
  * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
  * errors on the backend.
  */
-const t = initTRPC.context<typeof createTRPCContext>().create({
+const t = initTRPC.context<Context>().create({
+  /**
+   * @see https://trpc.io/docs/v11/data-transformers
+   */
   transformer: superjson,
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.cause instanceof ZodError ? error.cause.flatten() : null,
-      },
-    };
+  /**
+   * @see https://trpc.io/docs/v11/error-formatting
+   */
+  errorFormatter({ shape }) {
+    return shape;
+  },
+  sse: {
+    maxDurationMs: 5 * 60 * 1_000, // 5 minutes
+    ping: {
+      enabled: true,
+      intervalMs: 3_000,
+    },
+    client: {
+      reconnectAfterInactivityMs: 5_000,
+    },
   },
 });
 
@@ -68,12 +81,10 @@ export const createCallerFactory = t.createCallerFactory;
  *
  * These are the pieces you use to build your tRPC API. You should import these a lot in the
  * "/src/server/api/routers" directory.
- */
-
-/**
+ *
  * This is how you create new routers and sub-routers in your tRPC API.
  *
- * @see https://trpc.io/docs/router
+ * @see https://trpc.io/docs/v11/router
  */
 export const createTRPCRouter = t.router;
 
@@ -86,14 +97,22 @@ export const createTRPCRouter = t.router;
 const timingMiddleware = t.middleware(async ({ next, path }) => {
   const start = Date.now();
 
-  if (t._config.isDev) {
-    // artificial delay in dev
-    const waitMs = Math.floor(Math.random() * 400) + 100;
-    await new Promise((resolve) => setTimeout(resolve, waitMs));
+  if (process.env.NODE_ENV === "development") {
+    const randomNumber = (min: number, max: number) =>
+      Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const delay = randomNumber(120, 500);
+    console.debug(
+      "🚩 doing artificial delay of",
+      delay,
+      "ms before returning",
+      path,
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, delay));
   }
 
   const result = await next();
-
   const end = Date.now();
   console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
 
@@ -106,8 +125,14 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
  * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
  * guarantee that a user querying is authorized, but you can still access user session data if they
  * are logged in.
- */
+ * @see https://trpc.io/docs/v11/procedures
+ **/
 export const publicProcedure = t.procedure.use(timingMiddleware);
+
+/**
+ * @see https://trpc.io/docs/v11/merging-routers
+ */
+export const mergeRouters = t.mergeRouters;
 
 /**
  * Protected (authenticated) procedure
@@ -117,19 +142,23 @@ export const publicProcedure = t.procedure.use(timingMiddleware);
  *
  * @see https://trpc.io/docs/procedures
  */
-export const protectedProcedure = t.procedure
-  .use(timingMiddleware)
-  .use(({ ctx, next }) => {
-    if (!ctx.session?.user || !ctx.session?.user.id) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-      ctx: {
-        // infers the `session` as non-nullable and `user.id` as a string
-        session: {
-          ...ctx.session,
-          user: { ...ctx.session.user, id: ctx.session.user.id as string },
+export const protectedProcedure = publicProcedure.use(function isAuthed(opts) {
+  const user = opts.ctx.session?.user;
+
+  if (!user?.name) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    ctx: {
+      // infers the session and user as non-nullable
+      session: {
+        ...opts.ctx.session!,
+        user: {
+          ...user,
+          name: user.name as string,
         },
       },
-    });
+    },
   });
+});
