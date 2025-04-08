d981528384cce46ab107069fa223ddd5ebd6fe9f
Henning Sieh <admin@henningsieh.de>
2025-04-08 17:17:45 +0200

    fix: update translation keys for creation and update dates in plant card component

--- src/components/features/Plants/plant-card.tsx
+++ src/components/features/Plants/plant-card.tsx
@@ -137,7 +137,7 @@ export default function PlantCard({
   const dateElement = (
     <Link
       href={`${modulePaths.PUBLICPLANTS.path}/${plant.id}`}
-      title={t("plant-card-createdAt")}
+      title={t("created-at")}
       className="text-muted-foreground flex items-center gap-1 text-sm whitespace-nowrap"
     >
       {<DotIcon size={24} className="xs:block -mx-2 hidden" />}
